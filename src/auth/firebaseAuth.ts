29d3237d27d364af32570166abeb3eee1aee95ca
yashiroryo <>
2023-03-01 10:29:29 +0900

    fix: リリースを優先するために差し戻し

--- src/auth/firebaseAuth.ts
+++ src/auth/firebaseAuth.ts
@@ -1,9 +1,12 @@
 import * as admin from "firebase-admin";
+import * as serviceAccount from "../../keys/task-manage-app-1cb7a-firebase-adminsdk-mqez7-9784b5b4d1.json";
 import { getAuth } from "firebase-admin/auth";
 
 class FirebaseAuth {
   constructor() {
-    admin.initializeApp();
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    });
   }
 
   verifyFirebaseToken(token: string) {

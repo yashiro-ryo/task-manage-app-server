f6f336bf45fffab30a6ae24280cbfc2e75633823
yashiroryo <>
2023-03-01 10:08:58 +0900

    refactor: 環境変数からfirebaseの初期化情報を取得するように修正

--- src/auth/firebaseAuth.ts
+++ src/auth/firebaseAuth.ts
@@ -1,12 +1,9 @@
 import * as admin from "firebase-admin";
-import * as serviceAccount from "../../keys/task-manage-app-1cb7a-firebase-adminsdk-mqez7-9784b5b4d1.json";
 import { getAuth } from "firebase-admin/auth";
 
 class FirebaseAuth {
   constructor() {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    });
+    admin.initializeApp();
   }
 
   verifyFirebaseToken(token: string) {
